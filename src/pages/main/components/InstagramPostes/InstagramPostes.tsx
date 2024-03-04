da77ccb6cc5dbbcb2c055520ae0a6db875e2f158
hamzaAbdrahim <rsstrr7@gmail.com>
2024-03-04 16:28:32 +0100

    'push_site'

--- src/pages/main/components/InstagramPostes/InstagramPostes.tsx
+++ src/pages/main/components/InstagramPostes/InstagramPostes.tsx
@@ -4,7 +4,7 @@ import carImage from "../../../../../public/images/carImage.png";
 const InstagramPostes = () => {
   return (
     <section className={`${styles.h_screen_center} ${styles.BlackSection} overflow-hidden relative`}>
-      <div className={`${styles.flexitems} gap-3 ${styles.center__y} text-nowrap`}>
+      <div className={`${styles.flexitems} gap-3 ${styles.center__y} top-[50%] text-nowrap`}>
       <h3 className="mb-5 text-[40px] font-normal leading-[48px] tracking-normal">
         Instagram  -</h3>
         <h3 className="mb-5 text-[40px] font-normal leading-[48px] tracking-normal">
